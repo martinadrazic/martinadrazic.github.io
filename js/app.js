dde9e95d7344d25cfd075f276871d69940f32dc4
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-29 14:43:38 +0200

    project section changed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 $('.header-top a').on('click', function(event){
     event.preventDefault();
 
@@ -7,3 +5,32 @@ $('.header-top a').on('click', function(event){
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
 });
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
